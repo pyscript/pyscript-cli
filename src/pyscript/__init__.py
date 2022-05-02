a0c73bcdd0d9f41d181770fcf310beb106087059
Matt Kramer <mkramer@anaconda.com>
2022-05-01 20:53:30 -0500

    Load version number via importlib.metadata

--- src/pyscript/__init__.py
+++ src/pyscript/__init__.py
@@ -1,1 +1,8 @@
 """A CLI for PyScript!"""
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version("pyscript-cli")
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
