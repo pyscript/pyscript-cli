366240c072ee2bf70959efbced63403b1bbeb0eb
Nicholas H.Tollervey <ntoll@ntoll.org>
2022-08-11 10:55:41 +0100

    More mypy appeasement in the tests.

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -12,14 +12,14 @@ def test_new_project() -> None:
     gen.toml = mock.MagicMock()
     mock_path = mock.MagicMock()
     mock_path.__truediv__ = mock.MagicMock(return_value=mock_path)
-    gen.Path = mock.MagicMock(return_value=mock_path)
+    gen.Path = mock.MagicMock(return_value=mock_path)  # type: ignore
     gen.string_to_html = mock.MagicMock()
     app_name = "app_name"
     app_description = "A longer, human friendly, app description."
     author_name = "A.Coder"
     author_email = "acoder@domain.com"
     gen.new_project(app_name, app_description, author_name, author_email)
-    assert gen.toml.dump.call_count == 1
-    gen.string_to_html.assert_called_once_with(
+    assert gen.toml.dump.call_count == 1  # type: ignore
+    gen.string_to_html.assert_called_once_with(  # type: ignore
         'print("Hello, world!")', app_name, mock_path
     )
