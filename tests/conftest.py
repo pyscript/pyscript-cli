5604d5c8d0a53efe7e08cbd856df19d76c2ca9a0
Matt Kramer <mkramer@anaconda.com>
2022-08-24 12:11:15 -0500

    Refactor test to not use mocks

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from typing import Any
+
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+
+@pytest.fixture()
+def tmp_cwd(monkeypatch: MonkeyPatch, tmp_path: Path) -> Path:
+    """Create & return a temporary directory after setting current working directory to it."""
+    monkeypatch.chdir(tmp_path)
+    return tmp_path
+
+
+@pytest.fixture()
+def is_not_none() -> Any:
+    """
+    An object that can be used to test whether another is None.
+
+    This is particularly useful when testing contents of collections, e.g.:
+
+    ```python
+    def test_data(data, is_not_none):
+        assert data == {"some_key": is_not_none, "some_other_key": 5}
+    ```
+
+    """
+
+    class _NotNone:
+        def __eq__(self, other):
+            return other is not None
+
+    return _NotNone()
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,23 +3,33 @@ Tests for utility functions in the _generator.py module that cannot be
 exercised because of limitations in the Typer testing framework (specifically,
 multiple "prompt" arguments).
 """
-from unittest import mock
+from pathlib import Path
+from typing import Any
+
+import toml
 
 from pyscript import _generator as gen
 
 
-def test_create_project() -> None:
-    gen.toml = mock.MagicMock()
-    mock_path = mock.MagicMock()
-    mock_path.__truediv__ = mock.MagicMock(return_value=mock_path)
-    gen.Path = mock.MagicMock(return_value=mock_path)  # type: ignore
-    gen.string_to_html = mock.MagicMock()
+def test_create_project(tmp_cwd: Path, is_not_none: Any) -> None:
     app_name = "app_name"
     app_description = "A longer, human friendly, app description."
     author_name = "A.Coder"
     author_email = "acoder@domain.com"
     gen.create_project(app_name, app_description, author_name, author_email)
-    assert gen.toml.dump.call_count == 1  # type: ignore
-    gen.string_to_html.assert_called_once_with(  # type: ignore
-        'print("Hello, world!")', app_name, mock_path
-    )
+
+    manifest_path = tmp_cwd / app_name / "manifest.toml"
+    assert manifest_path.exists()
+
+    with manifest_path.open() as fp:
+        contents = toml.load(fp)
+
+    assert contents == {
+        "app_name": "app_name",
+        "app_description": "A longer, human friendly, app description.",
+        "author_name": "A.Coder",
+        "author_email": "acoder@domain.com",
+        "created_on": is_not_none,
+        "id": is_not_none,
+        "version": is_not_none,
+    }
