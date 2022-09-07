2e76c499e65d476b5c0de224f51a38ee10c05128
Valerio Maggio <leriomaggio@users.noreply.github.com>
2022-09-07 11:25:09 +0100

    docstring to new Warning typer class
    and removed useless code from prev. manual merge

--- src/pyscript/cli.py
+++ src/pyscript/cli.py
@@ -32,6 +32,10 @@ class Abort(typer.Abort):
 
 
 class Warning(typer.Exit):
+    """Warning with a consistent error message.
+    (Similar in essence to the Abort class - it only changes font color to yellow!)
+    """
+
     def __init__(self, msg: str, *args: Any, **kwargs: Any):
         console.print(msg, style="yellow")
         super().__init__(*args, **kwargs)
@@ -53,62 +57,6 @@ def main(
 
 pm = PluginManager("pyscript")
 
-# @app.command()
-# def wrap(
-#     input_file: Optional[Path] = _input_file_argument,
-#     output: Optional[Path] = _output_file_option,
-#     command: Optional[str] = _command_option,
-#     show: Optional[bool] = _show_option,
-#     title: Optional[str] = _title_option,
-# ) -> None:
-#     """Wrap a Python script inside an HTML file."""
-#     title = title or "PyScript App"
-#
-#     if not input_file and not command:
-#         raise Abort(
-#             "Must provide either an input '.py' file or a command with the '-c' option."
-#         )
-#     if input_file and command:
-#         raise Abort("Cannot provide both an input '.py' file and '-c' option.")
-#
-#     # Derive the output path if it is not provided
-#     remove_output = False
-#     if output is None:
-#         if command and show:
-#             output = Path("pyscript_tmp.html")
-#             remove_output = True
-#         elif not command:
-#             assert input_file is not None
-#             output = input_file.with_suffix(".html")
-#         else:
-#             raise Abort("Must provide an output file or use `--show` option")
-#
-#     if input_file is not None:
-#         parsing_res = file_to_html(input_file, title, output)
-#         if parsing_res is not None:
-#             msg_template = "WARNING: The input file contains some imports which are not currently supported PyScript.\nTherefore the code might not work, or require some changes.{packages}{locals}"
-#             msg = msg_template.format(
-#                 packages=f"\n{str(parsing_res.unsupported_packages)}"
-#                 if parsing_res.unsupported_packages
-#                 else "",
-#                 locals=f"\n{str(parsing_res.unsupported_paths)}"
-#                 if parsing_res.unsupported_paths
-#                 else "",
-#             )
-#             raise Warning(msg=msg)
-#
-#     if command:
-#         string_to_html(command, title, output)
-#
-#     assert output is not None
-#
-#     if show:
-#         console.print("Opening in web browser!")
-#         webbrowser.open(f"file://{output.resolve()}")
-#
-#     if remove_output:
-#         time.sleep(1)
-#         output.unlink()
 
 pm.add_hookspecs(hookspecs)
 for modname in DEFAULT_PLUGINS:
