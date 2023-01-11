5cede4c561803fa24a43d77594b456d7ceba7ba8
Fabio Pliger <fpliger@users.noreply.github.com>
2023-01-11 14:40:43 -0800

    Add explicit pyscript version to create and wrap cmd templates (#67)
    * add LATEST_PYSCRIPT_VERSION constant
    
    * add pyscript-version parameter to create cmd
    
    * add pyscript_version parameter on methods that create new app from template
    
    * add test for new create cmd version argument
    
    * add test for create
    
    * add test for wrap cmd
    
    * add test for pyscript version when calling wrap with file instead of cmd
    
    * improve tests
    
    * fix typing error
    
    Co-authored-by: Fabio Pliger <fpliger@anaconda.com>

--- src/pyscript/_generator.py
+++ src/pyscript/_generator.py
@@ -6,7 +6,7 @@ from typing import Optional
 import jinja2
 import toml
 
-from pyscript import config
+from pyscript import LATEST_PYSCRIPT_VERSION, config
 
 _env = jinja2.Environment(loader=jinja2.PackageLoader("pyscript"))
 TEMPLATE_PYTHON_CODE = """# Replace the code below with your own
@@ -15,7 +15,11 @@ print("Hello, world!")
 
 
 def create_project_html(
-    title: str, python_file_path: str, config_file_path: str, output_file_path: Path
+    title: str,
+    python_file_path: str,
+    config_file_path: str,
+    output_file_path: Path,
+    pyscript_version: str = LATEST_PYSCRIPT_VERSION,
 ) -> None:
     """Write a Python script string to an HTML file template."""
     template = _env.get_template("basic.html")
@@ -25,6 +29,7 @@ def create_project_html(
                 python_file_path=python_file_path,
                 config_file_path=config_file_path,
                 title=title,
+                pyscript_version=pyscript_version,
             )
         )
 
@@ -49,7 +54,11 @@ def save_config_file(config_file: Path, configuration: dict):
 
 
 def string_to_html(
-    code: str, title: str, output_path: Path, template_name: str = "basic.html"
+    code: str,
+    title: str,
+    output_path: Path,
+    template_name: str = "basic.html",
+    pyscript_version: str = LATEST_PYSCRIPT_VERSION,
 ) -> None:
     """Write a Python script string to an HTML file template.
 
@@ -59,14 +68,18 @@ def string_to_html(
                      PyScript app template
         - title(str): application title, that will be placed as title of the html
                       app template
+        - output_path(Path): path where to write the new html file
         - template_name(str): name of the template to be used
+        - pyscript_version(str): version of pyscript to be used
 
     Output:
         (None)
     """
     template = _env.get_template(template_name)
     with output_path.open("w") as fp:
-        fp.write(template.render(code=code, title=title))
+        fp.write(
+            template.render(code=code, title=title, pyscript_version=pyscript_version)
+        )
 
 
 def file_to_html(
@@ -74,11 +87,12 @@ def file_to_html(
     title: str,
     output_path: Optional[Path],
     template_name: str = "basic.html",
+    pyscript_version: str = LATEST_PYSCRIPT_VERSION,
 ) -> None:
     """Write a Python script string to an HTML file template."""
     output_path = output_path or input_path.with_suffix(".html")
     with input_path.open("r") as fp:
-        string_to_html(fp.read(), title, output_path, template_name)
+        string_to_html(fp.read(), title, output_path, template_name, pyscript_version)
 
 
 def create_project(
@@ -86,6 +100,7 @@ def create_project(
     app_description: str,
     author_name: str,
     author_email: str,
+    pyscript_version: str = LATEST_PYSCRIPT_VERSION,
 ) -> None:
     """
     New files created:
@@ -124,4 +139,5 @@ def create_project(
         config["project_main_filename"],
         config["project_config_filename"],
         index_file,
+        pyscript_version=pyscript_version,
     )
