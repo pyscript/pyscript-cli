4428a30cb80aebb8da929ca4bd904b4fd446cccd
Fabio Pliger <fpliger@anaconda.com>
2023-05-22 16:30:40 -0500

    move statement out of loop

--- src/pyscript/cli.py
+++ src/pyscript/cli.py
@@ -59,7 +59,9 @@ for modname in DEFAULT_PLUGINS:
     else:
         mod = sys.modules[importspec]
         pm.register(mod, modname)
-    loaded = pm.load_setuptools_entrypoints("pyscript")
+
+
+loaded = pm.load_setuptools_entrypoints("pyscript")
 
 for cmd in pm.hook.pyscript_subcommand():
     plugins._add_cmd(cmd)
